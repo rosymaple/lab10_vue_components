3bc0a0173c90f05bc37f4847fe500de32582ee76
rosymaple <candace.vogel@go.minneapolis.edu>
2024-12-06 16:41:43 -0600

    commented lab from class

---
+++ lab10_vue_components/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+import { ref } from 'vue'
+import WouldYouRather from './components/WouldYouRather.vue'
+// importing child components from WouldYouRather.vue
+
+// data for the question, and two answers
+const wyrQuestion = ref('Live in a house shaped like a triangle house or like a circle?')
+const answer1 = ref('Triangle house')
+const answer2 = ref('Circle house')
+
+// Will store the user's selection, either 'triangle house' or 'circle house'
+const userSelection = ref('')
+
+// a function with userChoice as our parameter
+// this function will serve to print a message for our user...
+// telling user which checkbox they chose.
+function updateUserSelection(userChoice) {
+  userSelection.value = `Thanks! You chose ${userChoice}!`
+}
+
+</script>
+
+<template>
+
+  <div id="app-component">
+
+  <h1>Would You Rather...</h1>
+
+    <!-- imported WouldYouRather component -->
+    <!-- binding question prop to wyrQuestion-->
+    <!-- answer-selected is an event -->
+  <WouldYouRather
+    v-bind:question = "wyrQuestion"
+    v-bind:answer1 = "answer1"
+    v-bind:answer2 = "answer2"
+    v-on:answer-selected="updateUserSelection">
+    </WouldYouRather>
+
+  <!-- TODO show user's choice from the would-you-rather question -->
+    <!-- if user makes a selection, this paragraph will appear-->
+    <p v-if="userSelection">{{ userSelection }}</p>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+#app-component {
+  /* font-size: 3em */
+  background-color: aqua;
+  padding: 40px;
+}
+
+</style>
---
+++ lab10_vue_components/src/components/WouldYouRather.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+// import watch to react to reactive data in our program
+
+// props defined for our program. this will replace our usual
+// return statements.
+
+defineProps({
+  question: String,
+  answer1: String,
+  answer2: String
+})
+
+// we are using emit to communicate child to parent components...
+// such as changes in the child components state
+const emit = defineEmits(['answer-selected'])
+
+// empty reactive reference
+const choice = ref('')
+
+// function choiceMade() {
+//   // emit event to tell parent that user has made choice.
+//   emit('answer-selected', choice.value)
+// }
+
+// the watch function will detect changes in "choice"...
+// and change answer-selected to the changed choice value
+watch(choice, () => {
+  emit('answer-selected', choice.value)
+})
+
+</script>
+
+<template>
+
+  <div id="wyr">
+    <h2>Make your choice!</h2>
+
+    <!-- displaying question prop -->
+    <h3>{{ question }}</h3>
+
+    <div>
+      <!-- must v-model to the choice variable -->
+      <input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1">
+      <label for="answer-1"> {{ answer1 }}</label>
+
+      <!-- the value attribute is the HTML value -->
+      <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
+      <label for="answer-2"> {{ answer2 }}</label>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+/* background for wyr div */
+#wyr {
+  background-color: hotpink;
+}
+
+p {
+  font-family: fantasy;
+}
+
+</style>
